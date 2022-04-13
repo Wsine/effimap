764f5aadec4a7909240ad8a8470f03c25ef9018e
Wsine <weizhy3@hotmail.com>
2022-04-13 15:49:04 +0800

    useless multi mutation score

--- prioritize.py
+++ prioritize.py
@@ -1,3 +1,5 @@
+import copy
+
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm
@@ -6,7 +8,8 @@ from sklearn import metrics
 
 from dataset import load_dataloader
 from model import get_device, load_model
-from mutate import feature_hook, feature_container
+from models.vanilla_vae import VanillaVAE
+from mutate import feature_hook, feature_container, channel_hook
 from arguments import parser
 from utils import *
 
@@ -47,9 +50,9 @@ def gini_method(_, model, dataloader, device):
     print('auc for gini: {:.2f}%'.format(100. * auc))
 
 
-@dispatcher.register('furret')
+@dispatcher.register('furret-1')
 @torch.no_grad()
-def estimator_method(opt, model, dataloader, device):
+def estimator_with_mutation_score(opt, model, dataloader, device):
     for module in model.modules():
         module.register_forward_hook(feature_hook)
     model.eval()
@@ -85,8 +88,69 @@ def estimator_method(opt, model, dataloader, device):
     print('auc for mutation score: {:.2f}%'.format(100. * auc))
 
 
+@dispatcher.register('furret-2')
+@torch.no_grad()
+def estimator_with_multi_mutation_score(opt, model, dataloader, device):
+    model.eval()
+    mutated_model = copy.deepcopy(model)
+    mutate_info = load_object(opt, 'model_mutants_info.json')
+    for k, v in mutate_info.items():  # type: ignore
+        mutant_idx = int(k[len('mutant'):])  # type: ignore
+        lname, chn_idx = v['layer_name'], v['channel_idx']
+        module = rgetattr(mutated_model, lname)
+        module.register_forward_hook(channel_hook(chn_idx, mutant_idx - 1))
+    for module in mutated_model.modules():
+        module.register_forward_hook(feature_hook)
+    mutated_model.eval()
+
+    img_size = next(iter(dataloader))[0].size(-1)
+    generator = VanillaVAE(3, img_size, 10)
+    state = load_object(opt, 'encoder_model.pt')
+    generator.load_state_dict(state['net'])  # type: ignore
+    generator = generator.to(device)
+    generator.eval()
+
+    mutation_model = load_object(opt, 'mutation_estimator.pkl')
+
+    df = pd.DataFrame()
+    for inputs, targets in tqdm(dataloader, desc='Validate'):
+        inputs, targets = inputs.to(device), targets.to(device)
+        _, predicted = model(inputs).max(1)
+        equals = predicted.eq(targets)
+
+        input_features_pool, model_features_pool = [], []
+        for idx, input in enumerate(tqdm(inputs, desc='Samples', leave=False)):
+            input_repeat = input.view(1, *input.size()) \
+                    .repeat(opt.num_input_mutants, 1, 1, 1)
+            _, pred = model(input_repeat).max(1)
+            input_features_pool.append(pred.ne(predicted[idx]).cpu())
+
+            input_repeat = input.view(1, *input.size()) \
+                    .repeat(opt.num_model_mutants, 1, 1, 1)
+            input_mutants = generator.generate(input_repeat)
+            feature_container.clear()
+            mutated_model(input_mutants)
+            features = torch.stack(feature_container, dim=-1).cpu()
+            model_features_pool.append(features)
+        model_features_pool = torch.cat(model_features_pool)
+
+        mutation = mutation_model.predict(model_features_pool.numpy())  # type: ignore
+        for i in range(equals.size(0)):
+            f1 = input_features_pool[i].sum().item()
+            f2 = mutation[i*opt.num_model_mutants:(i+1)*opt.num_model_mutants].sum()
+            df = df.append({
+                'mutation_score': f1 + f2,
+                'actual': (~equals[i]).item()
+            }, ignore_index=True)
+
+    df = df.astype({'mutation_score': float, 'actual': int})
+    fpr, tpr, _ = metrics.roc_curve(df['actual'], df['mutation_score'])
+    auc = metrics.auc(fpr, tpr)
+    print('auc for mutation score: {:.2f}%'.format(100. * auc))
+
+
 @dispatcher.register('prima')
-def prima_method(opt, model, dataloader, device):
+def prima_method(opt, *_):
     input_features = load_object(opt, 'prima_input_features_test.pt')
     model_features = load_object(opt, 'prima_model_features_test.pt')
     feature_target = load_object(opt, 'prima_feature_target_test.pt')
