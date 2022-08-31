d93ee3c5a7c9ba9beb09ee491ff62316e0e475f6
Wsine <weizhy3@hotmail.com>
2022-08-31 14:48:22 +0800

    fix prima features

--- prima_prioritize.py
+++ prima_prioritize.py
@@ -6,21 +6,38 @@ from arguments import parser
 from model import load_model
 from dataset import load_dataloader
 from metric import correctness, post_predict
-from generate_mutants import generate_random_sample_mutants
-from train_pmt_ranker import compute_prima_sample_feature
+from generate_mutants import InverseActivate
+from train_prima_ranker import compute_prima_samples_features
+from train_prima_ranker import extract_prima_model_mutants_predictions
+from train_prima_ranker import extract_prima_sample_mutants_predictions
 from utils import get_device, guard_folder, load_pickle_object, save_object
 
 
-def get_prima_batch_features(ctx, inputs, model, device):
-    sample_features = []
-    for sample_with_mutants in generate_random_sample_mutants(ctx, inputs):
-        inputs = sample_with_mutants.to(device)
-        inputs_preds = post_predict(ctx, model(inputs))
-        feature = compute_prima_sample_feature(ctx, inputs_preds)
-        feature = feature.cpu()
-        sample_features.append(feature)
-    sample_features = torch.stack(sample_features).numpy()
+@torch.no_grad()
+def extract_prima_sample_features(ctx, model, valloader, device):
+    model_mutants_preds = \
+        extract_prima_model_mutants_predictions(ctx, valloader, device)
 
+    sample_mutants_preds = \
+        extract_prima_sample_mutants_predictions(ctx, model, valloader, device)
+
+    sample_features = []
+    if ctx.task == 'clf':
+        model_mutants_labels, model_mutants_probs = model_mutants_preds
+        sample_mutants_labels, sample_mutants_probs = sample_mutants_preds
+        for mm_labels, mm_probs, sm_labels, sm_probs in zip(
+                model_mutants_labels, model_mutants_probs,
+                sample_mutants_labels, sample_mutants_probs):
+            input_pred = (sm_labels[0], sm_probs[0])
+            samples_pred = (sm_labels[1:], sm_probs[1:])
+            models_pred = (mm_labels, mm_probs)
+            feature = compute_prima_samples_features(
+                ctx, input_pred, samples_pred, models_pred)
+            sample_features.append(feature)
+    else:
+        raise NotImplemented
+
+    sample_features = torch.stack(sample_features).numpy()
     return sample_features
 
 
@@ -31,25 +48,21 @@ def prioritize_by_prima(ctx):
     model = load_model(ctx, pretrained=True).to(device)
     model.eval()
 
+    sample_features = extract_prima_sample_features(ctx, model, testloader, device)
 
     ranker = load_pickle_object(ctx, f'prima_ranker.pkl')
     assert(ranker is not None)
 
-    model_errors, oracle = [], []
+    model_errors = ranker.predict(sample_features)
+    print(model_errors.shape)
+
+    oracle = []
     for inputs, targets in tqdm(testloader, desc='Batch'):
-        inputs_preds = post_predict(ctx, model(inputs.to(device)))
-
-        batch_features = get_prima_batch_features(ctx, inputs, model, device)
-
-        pred_errors = ranker.predict(batch_features)
-        model_errors.append(pred_errors)
-
-        targets = targets.to(device)
+        inputs, targets = inputs.to(device), targets.to(device)
+        inputs_preds = post_predict(ctx, model(inputs))
         incorrect = correctness(ctx, inputs_preds, targets, invert=True)
         oracle.append(incorrect)
 
-    model_errors = np.concatenate(model_errors)
-    print(model_errors.shape)
     oracle = torch.cat(oracle).numpy()
     print(oracle.shape)
 
--- train_prima_ranker.py
+++ train_prima_ranker.py
@@ -8,8 +8,8 @@ from arguments import parser
 from metric import post_predict, prediction_error
 from model import load_model
 from dataset import load_dataloader
-from generate_mutants import generate_random_sample_mutants
-from utils import get_device, load_pickle_object, save_object
+from generate_mutants import InverseActivate, generate_random_sample_mutants
+from utils import get_device, load_pickle_object, load_torch_object, save_object
 
 
 @torch.no_grad()
@@ -25,25 +25,103 @@ def extract_model_errors(ctx, model, valloader, device):
     return pred_errors
 
 
-def compute_prima_sample_feature(ctx, inputs_preds):
-    if ctx.task == 'clf':
-        pred_labels, pred_probs = inputs_preds
-        sample_label, mutant_labels = pred_labels[0], pred_labels[1:]
-        sample_prob, mutant_probs = pred_probs[0], pred_probs[1:]
+@torch.no_grad()
+def extract_prima_model_mutants_predictions(ctx, valloader, device):
+    all_mutants_preds = []
+    for mutant_idx in tqdm(range(ctx.num_model_mutants)):
+        mutant_path = f'model_mutants/random_mutant.{mutant_idx}.pt'
+        mutant = load_torch_object(ctx, mutant_path)
+        assert(mutant is not None)
+        mutant = mutant.to(device)
+        mutant.eval()
 
-        f1a = mutant_labels.ne(sample_label).sum()
-        f1b = mutant_labels.unique().size(0) - 1
-        _, cnt = mutant_labels.unique(return_counts=True)
-        f1c = cnt[cnt.topk(2).indices[-1]] if cnt.size(0) > 1 else cnt[0]
-        dist = F.cosine_similarity(
-            mutant_probs, sample_prob.repeat(mutant_probs.size(0), 1))
-        f2a = dist.mean()
-        f2b = [
-            torch.logical_and(dist > s, dist < s + 0.1).sum()
-            for s in torch.linspace(0, 1, steps=10)
+        mutant_preds_list = []
+        for inputs, targets in tqdm(valloader, desc='Eval', leave=False):
+            inputs, targets = inputs.to(device), targets.to(device)
+            mutant_preds = post_predict(ctx, mutant(inputs))
+            mutant_preds_list.append(mutant_preds)
+
+        if ctx.task == 'clf':
+            mutant_pred_labels = torch.cat([label for label, _ in mutant_preds_list])
+            mutant_pred_probs = torch.cat([prob for _, prob in mutant_preds_list])
+            all_mutants_preds.append(
+                (mutant_pred_labels.cpu(), mutant_pred_probs.cpu())
+            )
+        else:
+            raise NotImplemented
+
+    if ctx.task == 'clf':
+        all_mutants_labels = torch.stack([
+            labels for labels, _ in all_mutants_preds
+        ], dim=-1)
+        print('all_mutants_labels -', all_mutants_labels.size())
+        all_mutants_probs = torch.stack([
+            probs for _, probs in all_mutants_preds
+        ], dim=-1)
+        print('all_mutants_probs -', all_mutants_probs.size())
+        return all_mutants_labels, all_mutants_probs
+    else:
+        raise NotImplemented
+
+
+@torch.no_grad()
+def extract_prima_sample_mutants_predictions(ctx, model, valloader, device):
+    all_mutants_preds = []
+    for inputs, _ in tqdm(valloader, desc='Batch'):
+        for sample_with_mutants in generate_random_sample_mutants(ctx, inputs):
+            inputs = sample_with_mutants.to(device)
+            inputs_preds = post_predict(ctx, model(inputs))
+            all_mutants_preds.append(inputs_preds)
+
+    if ctx.task == 'clf':
+        all_mutants_labels = torch.stack([
+            labels for labels, _ in all_mutants_preds
+        ]).cpu()
+        print('all_mutants_labels -', all_mutants_labels.size())
+        all_mutants_probs = torch.stack([
+            probs for _, probs in all_mutants_preds
+        ]).cpu()
+        print('all_mutants_probs -', all_mutants_probs.size())
+        return all_mutants_labels, all_mutants_probs
+    else:
+        raise NotImplemented
+
+
+def compute_prima_samples_features(ctx, input_pred, samples_pred, models_pred):
+    if ctx.task == 'clf':
+        input_label, input_probs = input_pred
+        samples_labels, sample_probs = samples_pred
+        models_labels, models_probs = models_pred
+
+        f1a = [
+            models_labels.ne(input_label).sum(),
+            samples_labels.ne(input_label).sum()
         ]
-        f2c = (mutant_probs[:, sample_label] - sample_prob[sample_label]).mean()
-        feature = torch.Tensor([f1a, f1b, f1c, f2a, *f2b, f2c])
+        f1b = [
+            models_labels.unique().size(0) - 1,
+            samples_labels.unique().size(0) - 1
+        ]
+        _, mm_cnt = models_labels.unique(return_counts=True)
+        _, sm_cnt = samples_labels.unique(return_counts=True)
+        f1c = [
+            mm_cnt[mm_cnt.topk(2).indices[-1]] if mm_cnt.size(0) > 1 else mm_cnt[0],
+            sm_cnt[sm_cnt.topk(2).indices[-1]] if sm_cnt.size(0) > 1 else sm_cnt[0]
+        ]
+        dist_func = lambda a, b: F.cosine_similarity(a, b.repeat(a.size(0), 1))
+        mm_dist = dist_func(models_probs, input_probs)
+        sm_dist = dist_func(sample_probs, input_probs)
+        f2a = [mm_dist.mean(), sm_dist.mean()]
+        iterval_func = lambda d, s: torch.logical_and(d > s, d < s + 0.1).sum()
+        mm_interval = [iterval_func(mm_dist, s) for s in torch.linspace(0, 1, steps=10)]
+        sm_interval = [iterval_func(sm_dist, s) for s in torch.linspace(0, 1, steps=10)]
+        f2b = [*mm_interval, *sm_interval]
+        f2c_func = lambda a, b: (a - b).abs().mean()
+        f2c = [
+            f2c_func(models_probs[input_label], input_probs[input_label]),
+            f2c_func(sample_probs[input_label], input_probs[input_label]),
+        ]
+        feature = torch.Tensor([*f1a, *f1b, *f1c, *f2a, *f2b, *f2c])
+        assert(feature.size(0) == 30)
     else:
         raise NotImplemented
     return feature
@@ -51,15 +129,33 @@ def compute_prima_sample_feature(ctx, inputs_preds):
 
 @torch.no_grad()
 def extract_prima_sample_features(ctx, model, valloader, device):
-    sample_features = []
+    model_mutants_preds = load_torch_object(ctx, 'prima_model_mutants_preds.pt')
+    if not model_mutants_preds:
+        model_mutants_preds = \
+            extract_prima_model_mutants_predictions(ctx, valloader, device)
+        save_object(ctx, model_mutants_preds, 'prima_model_mutants_preds.pt')
 
-    for inputs, _ in tqdm(valloader, desc='Batch'):
-        for sample_with_mutants in generate_random_sample_mutants(ctx, inputs):
-            inputs = sample_with_mutants.to(device)
-            inputs_preds = post_predict(ctx, model(inputs))
-            feature = compute_prima_sample_feature(ctx, inputs_preds)
-            feature = feature.cpu()
+    sample_mutants_preds = load_torch_object(ctx, 'prima_sample_mutants_preds.pt')
+    if not sample_mutants_preds:
+        sample_mutants_preds = \
+            extract_prima_sample_mutants_predictions(ctx, model, valloader, device)
+        save_object(ctx, sample_mutants_preds, 'prima_sample_mutants_preds.pt')
+
+    sample_features = []
+    if ctx.task == 'clf':
+        model_mutants_labels, model_mutants_probs = model_mutants_preds
+        sample_mutants_labels, sample_mutants_probs = sample_mutants_preds
+        for mm_labels, mm_probs, sm_labels, sm_probs in zip(
+                model_mutants_labels, model_mutants_probs,
+                sample_mutants_labels, sample_mutants_probs):
+            input_pred = (sm_labels[0], sm_probs[0])
+            samples_pred = (sm_labels[1:], sm_probs[1:])
+            models_pred = (mm_labels, mm_probs)
+            feature = compute_prima_samples_features(
+                ctx, input_pred, samples_pred, models_pred)
             sample_features.append(feature)
+    else:
+        raise NotImplemented
 
     sample_features = torch.stack(sample_features).numpy()
     save_object(ctx, sample_features, f'prima_features.pkl')
